4f68777274cd4783b43261e85081dd5df06058b4
irfan arya <irfanbasitali23@gmail.com>
2019-02-05 11:11:47 +0500

    Basic  tensorflow
    help to understand the basic of the tensorflow

---
+++ tfbacise.py
@@ -0,0 +1,17 @@
+import tensorflow as tf 
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+#result = x1 * x2
+result = tf.multiply(x1, x2)
+print(result)
+
+
+
+
+with tf.Session() as sess:
+	output = sess.run(result)
+	print(result)
+
+print(output)
